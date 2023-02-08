475a620da63f1a4b0fe59135d066ee1b1cea8f33
minyoungshin <mins6649@gmail.com>
2023-02-07 21:50:06 -0500

    event listener button

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+  
+    button.addEventListener("click", () => {
+        alert("I was clicked!")
+    });
 }
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+addingEventListener();
